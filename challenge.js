0ea23d673dfb564a12e5be7a5cb03fe46f0bc780
Abby Nyakara <81410040+AbbyNyakara@users.noreply.github.com>
2022-07-15 21:49:17 +0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,1 +1,15 @@
+module.exports = function move(starting, goal) {
+    // move the smaller one at the starting to a number that's nether the starting or the goal.
+    // Remain with the bigger one that goes at the bottom
+    // Move the bigger one to the goal;
+    // Move the smaller one to the goal;
+  let intermediate = [1,2,3].filter(value => ![starting,goal].includes(value));
+  return `${starting}->${intermediate} ${starting}->${goal} ${intermediate}->${goal}`
+}
 
+console.log(move(1, 3))
+// => 1->2 1->3 2->3
+
+console.log(move(2, 3))
+// => 2->1 2->3 1->3
+}
